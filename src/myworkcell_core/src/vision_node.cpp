bb05b5c9e2fdf528f9e5448e57107a9056c74d5f
jordy-u <jordyubink@hotmail.nl>
2020-02-13 07:27:19 -0600

    First commit with example ros-node.

---
+++ src/myworkcell_core/src/vision_node.cpp
@@ -0,0 +1,21 @@
+
+
+/**
+**  Simple ROS Node
+**/
+#include <ros/ros.h>
+
+int main(int argc, char* argv[])
+{
+  // This must be called before anything else ROS-related
+  ros::init(argc, argv, "vision_node");
+
+  // Create a ROS node handle
+  ros::NodeHandle nh;
+
+  ROS_INFO("Hello, World!");
+  ROS_ERROR("This is a test error!");
+
+  // Don't exit the program.
+  ros::spin();
+}
